f7bab6ec71b8ba96c29350e7316564f875890294
alxSource <alexander.hoglund@gmail.com>
2022-05-19 21:44:20 +0200

    up

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,9 +1,9 @@
 function Nav(props){
     return(
         <div key={props.nav_id} className="grid grid-cols-3 gap-1">
-            <span className="mr-5">{Number(props.nav_value).toFixed(2)}</span>
-            <span className="mr-5">{Number(props.total_shares).toFixed(2)}</span>
-            <span className="mr-5">{Number(props.nav_per_share).toFixed(4)}</span>
+            <span className="mr-5">{Number(props.nav_value).toFixed(1)}</span>
+            <span className="mr-5">{Number(props.total_shares).toFixed(1)}</span>
+            <span className="mr-5">{Number(props.nav_per_share).toFixed(2)}</span>
         </div>
     )
 }
